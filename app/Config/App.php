5e3601e887fe91e59c1080884a0169b5d04a9312
Richcy Dian Sukma <richcyveliz@gmail.com>
2024-04-22 12:41:41 +0700

    cloning project

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/webci4/';
+    public $baseURL = 'http://localhost/compro-codeigniter-4/';
 
     /**
      * --------------------------------------------------------------------------
--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'javawebmedia_webci4',
+        'database' => 'db_web_rsud',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
